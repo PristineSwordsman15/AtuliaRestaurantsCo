7f6e9160de9bef075862981ddabc0c2ccc4e3507
Atul Herle <ac122381@avcol.school.nz>
2024-10-16 01:46:15 +1300

    Created induvidual accounts project Added Models ApplicationUser,Category,Ingredient,Order;OrderItem,Product,ProductIngredient Changed ConnrctionStringName to AtuliaRestaurantsCoDb

---
+++ AtuliaRestaurantsCo/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AtuliaRestaurantsCo.Models
+
+   
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ AtuliaRestaurantsCo/Models/Ingredient.cs
@@ -0,0 +1,9 @@
+﻿namespace AtuliaRestaurantsCo.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string IngredientName { get; set; }
+        public ICollection<ProductIngredient> ProductIngredients { get; set; }
+    }
+}
