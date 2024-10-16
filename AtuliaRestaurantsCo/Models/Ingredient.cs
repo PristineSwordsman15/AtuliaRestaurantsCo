2b7c56f78f7a8b25c964f6e686fd08c922cdbc88
Atul Herle <ac122381@avcol.school.nz>
2024-10-17 01:05:51 +1300

    added validate never to Ingredient and product models as I never want those fields to be validated and placed a notmapped validation to have those fields public IFromFile? ImageFile and public string ImageUrl so a column isn;'t created in their respected table for them Added migration for the addition of ImageUrl

--- AtuliaRestaurantsCo/Models/Ingredient.cs
+++ AtuliaRestaurantsCo/Models/Ingredient.cs
@@ -1,9 +1,12 @@
-﻿namespace AtuliaRestaurantsCo.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace AtuliaRestaurantsCo.Models
 {
     public class Ingredient
     {
         public int IngredientId { get; set; }
         public string IngredientName { get; set; }
+        [ValidateNever]
         public ICollection<ProductIngredient> ProductIngredients { get; set; }
     }
 }
